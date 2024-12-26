ea2410fdb01b0478d46ea2f6459c54a0b2ac74a7
Abhijot Singh <abhijotsinghgtbit@gmail.com>
2024-12-26 18:18:46 +0530

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -52,7 +52,7 @@ console.log(    );
 console.log(    );
 
 
-let someNumber = 60
+let someNumber = 64
 
 
 let stringNumber = String(someNumber)      // now, someNumber number is converted to string
